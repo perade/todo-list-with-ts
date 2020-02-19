49d274f91f18177cd2693867e927c8fb162e215d
perade <perade0530@gmail.com>
2020-02-19 21:16:03 +0900

    init the project and set configures

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: {
+    'main': './src/main.ts'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: ['ts-loader']
+      },
+      {
+        test: /\.js&/,
+        use: ['source-map-loader'],
+        enforce: 'pre'
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: path.resolve(__dirname, 'dist')
+            }
+          },
+          // 'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'styles.css'
+    })
+  ],
+  resolve: {
+    extensions: ['.js', '.ts']
+  },
+  mode: 'development'
+};
